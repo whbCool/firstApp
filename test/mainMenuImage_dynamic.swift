6b179eb3a8897f4b16c283aeabe6c8f482f6e265
William D <whbcool1998@gmail.com>
2017-01-29 10:55:11 -0800

    Accelerometer Updates
    Accelerometer propeties are sorted.
    
    UI is stages for variables. UIImage is set in place. Buttons/Labels are set in place. Awating artowrk.

---
+++ test/mainMenuImage_dynamic.swift
@@ -0,0 +1,16 @@
+//
+//  mainMenuImage_dynamic.swift
+//  test
+//
+//  Created by Will D. on 1/29/17.
+//  Copyright © 2017 Will D. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MetalKit
+
+class mainMenu: UIViewController {
+    //to be discarded?
+    
+}
